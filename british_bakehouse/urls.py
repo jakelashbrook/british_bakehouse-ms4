b0635ea6b34497a944f8c35e442abc62eb3f7a06
jakelashbrook <jakeashleylashbrook@gmail.com>
2022-02-21 17:54:59 +0000

    Initial allauth setup

--- british_bakehouse/urls.py
+++ british_bakehouse/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth.urls import urlpatterns
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
 ]
